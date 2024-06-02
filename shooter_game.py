3bace6983ca2c4280f076c937889acf7915179c8
i am gay <154062090+fhtgrjlirtjgv@users.noreply.github.com>
2024-06-02 17:26:15 +0300

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, randint
 from pygame import *
 
 init()
@@ -24,7 +24,7 @@ bg_y1 = 0
 bg_y2 = -HEIGHT
 
 player_img = image.load("spaceship.png")
-
+enemy_img = image.load("alien.png")
 all_sprites = sprite.Group()
 
 class Sprite(sprite.Sprite):
@@ -41,8 +41,8 @@ class Player(Sprite):
     def __init__(self, sprite_img, width, height, x, y):
         super().__init__(sprite_img, width, height, x, y)
         self.hp = 100
-        self.speed = 3
-        self.bg_speed = 3
+        self.speed = 4
+        self.bg_speed = 4
         self.max_speed = 30
 
     def update(self):
@@ -65,15 +65,27 @@ class Player(Sprite):
 
 
 class Enemy(Sprite):
-    def __init__(self, sprite_img, width, height, x, y):
-        super().__init__(sprite_img, width, height, x, y)
+    def __init__(self, sprite_img, width, height):
+        rand_x = randint(0, WIDTH-width)
+        super().__init__(sprite_img, width, height, rand_x, -200)
         self.damage = 100
-        self.speed = 2        
+        self.speed = 3.5  
+        enemys.add(self)
+
+    def update(self):
+        self.rect.y += player.bg_speed +2
+        if self.rect.y > HEIGHT:
+            self.kill()
+
 
 
 player = Player(player_img, 100, 70, 300, 300)
 enemys = sprite.Group()
+enemy1 = Enemy(enemy_img, 80, 60)
 
+start_time = time.get_ticks()
+enemy_spawn_time = time.get_ticks()
+spawn_interval = randint(500, 1500)
 run = True
 finish = False
 
@@ -96,10 +108,20 @@ while run:
     if player.hp <= 0:
         finish = True
 
+    now = time.get_ticks()
+    if now - enemy_spawn_time > spawn_interval:
+        enemy2 = Enemy(enemy_img, 80, 60)
+        enemy_spawn_time = time.get_ticks()
+        spawn_interval = randint(500, 1500)
+
+    collide_list = sprite.spritecollide(player, enemys, False, sprite.collide_mask)
+    if len(collide_list) > 0:
+        finish = True
+
     all_sprites.draw(window)
     if not finish:
         all_sprites.update()
     if finish:
-        window.blit(game_over_text, (300, 300))
+        window.blit(game_over_text, (WIDTH/2 - game_over_text.get_width()/2, 300))
     display.update()
     clock.tick(FPS)
